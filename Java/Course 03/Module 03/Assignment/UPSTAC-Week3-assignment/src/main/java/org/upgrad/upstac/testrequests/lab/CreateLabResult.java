47e52d82d02fefebfc6cd43d3fb36ab2a472c6bb
Abhiroop Ghosh <abhiroop1107@gmail.com>
2021-06-15 08:51:42 +0000

    Changes to the test project of consultation controller and lab request controller

--- Java/Course 03/Module 03/Assignment/UPSTAC-Week3-assignment/src/main/java/org/upgrad/upstac/testrequests/lab/CreateLabResult.java
+++ Java/Course 03/Module 03/Assignment/UPSTAC-Week3-assignment/src/main/java/org/upgrad/upstac/testrequests/lab/CreateLabResult.java
@@ -1,10 +1,14 @@
 package org.upgrad.upstac.testrequests.lab;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Getter
+@Setter
 public class CreateLabResult {
 
     @NotNull
